569669bc4744e9fc6f5a1e4d7a9a01860e40ea8b
Tarun <tpunnoose@users.noreply.github.com>
2020-08-06 20:17:06 -0400

    removing extra files + dpes

--- src/Simulator/run_sim.jl
+++ src/Simulator/run_sim.jl
@@ -1,21 +1,21 @@
 using StaticArrays
 
-include("../src/Common/QuadrupedDynamics.jl")
-include("../src/Common/MPCControl/MPCControl.jl")
-include("../src/Common/Utilities.jl")
-include("../src/Common/Config.jl")
-include("../src/Simulator/XMLParser.jl")
+include("../Common/QuadrupedDynamics.jl")
+include("../Common/MPCControl/MPCControl.jl")
+include("../Common/Utilities.jl")
+include("../Common/Config.jl")
+include("XMLParser.jl")
 
 import .MPCControl
 using MuJoCoSimulator
 
-function test_sim()
-    yaml_path = joinpath(@__DIR__, "../src/Simulator/Simulator.yaml")
+function run_sim()
+    yaml_path = joinpath(@__DIR__, "Simulator.yaml")
     simulator_yaml = YAML.load(open(joinpath(yaml_path)))
 
     woofer = WooferConfig()
     ParseXML(woofer)
-    xml_path = yaml_path = joinpath(@__DIR__, "../src/Simulator/woofer_out.xml")
+    xml_path = yaml_path = joinpath(@__DIR__, "woofer_out.xml")
 
     # Initialize simulator object
     control_timestep = simulator_yaml["simulator"]["sim_data_dt"]
@@ -32,3 +32,5 @@ function test_sim()
 
     MuJoCoSimulator.simulate(sim, control!)
 end
+
+run_sim()
